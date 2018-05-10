814d9adb0690fb92f76067abf927244b98eb5f4f
Grzegorz Kalarus <kalarus.g@gmail.com>
2018-05-10 21:31:35 +0200

    cats

--- src/app.js
+++ src/app.js
@@ -6,12 +6,12 @@ import './styles/styles.scss';
 document.addEventListener('DOMContentLoaded', function(){
 
     const kitties = [
-        {category: "male", age: "4", likesKids: true, name: "Fidel Catstro"},
+        {category: "male", age: "4", likesKids: true, name: "Fantomcat"},
         {category: "male", age: "9", likesKids: true, name: "Hairy Potter"},
-        {category: "male", age: "2", likesKids: false, name: "Grumpy"},
-        {category: "female", age: "1", likesKids: true, name: "Jude Paw"},
-        {category: "female", age: "2", likesKids: false, name: "Lucifurr"},
-        {category: "female", age: "3", likesKids: true, name: "Meowly Cyrus"}
+        {category: "male", age: "2", likesKids: false, name: "Mr. Bigglesworth"},
+        {category: "female", age: "1", likesKids: true, name: "Tom Cat"},
+        {category: "female", age: "2", likesKids: false, name: "Bad Cat"},
+        {category: "female", age: "3", likesKids: true, name: "Chococat"}
     ];
 
     ReactDOM.render(
